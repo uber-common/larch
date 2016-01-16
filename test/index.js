cb4314086a9b549a0b4edbbf800d9da1bda5ad08
Raynos <raynos2@gmail.com>
2016-01-16 15:44:05 -0800

    comply with strict rule

--- test/index.js
+++ test/index.js
@@ -18,6 +18,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+'use strict';
+
 require('./errors');
 require('./larch');
 require('./level-router-backend');
